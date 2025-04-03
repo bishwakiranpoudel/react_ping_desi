c57232882b34c8c4e4ebddd5c04eeef727263074
Bishwa <bishwakiran725@gmail.com>
2025-04-03 19:55:18 +0545

    Implemented Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import {ToastContainer} from "react-toastify";
-import HomePage from "./Pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { LocationProvider } from "./context/location-context";
-// Remove the CSS import that's causing the error
+
+import { Home } from "react-feather";
+import HomePage2 from "./Pages/HomePage";
+import DiscoverPage from "./Pages/DiscoverPage";
 
 function App() {
   return (
     <LocationProvider>
-      <ToastContainer />
-      <HomePage />
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<HomePage2 />} />
+          <Route path="/discover" element={<DiscoverPage />} />
+          {/* Add more routes as needed */}
+          <Route path="*" element={<HomePage2 />} />
+        </Routes>
+      </Router>
     </LocationProvider>
   );
 }
