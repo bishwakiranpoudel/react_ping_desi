06a590034fdd31af6aee6ea66dd9d1bd20b04f43
sandeshsitaula <navaltajohan@gmail.com>
2025-04-03 18:09:35 +0545

    little cleanup of uneeded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import HomePage from "./Pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import { LocationProvider } from "./context/location-context";
 // Remove the CSS import that's causing the error
 
@@ -7,7 +13,13 @@ function App() {
   return (
     <LocationProvider>
       <ToastContainer />
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/signin" element={<HomePage />} />
+          <Route path="/signup" element={<HomePage />} />
+        </Routes>
+      </Router>
     </LocationProvider>
   );
 }
