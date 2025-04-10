353aa367b0d74a56939b41b641da6680b90b29da
Bishwa <bishwakiran725@gmail.com>
2025-04-11 02:55:51 +0545

    color

--- src/components/scoops_components/FAB.jsx
+++ src/components/scoops_components/FAB.jsx
@@ -6,7 +6,7 @@ function FloatingActionButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-[4.5rem] right-[1.5rem] w-14 h-14 rounded-full bg-black flex items-center justify-center shadow-md border-none cursor-pointer"
+      className="fixed bottom-[4.5rem] right-[1.5rem] w-14 h-14 rounded-full bg-[#7B189F] flex items-center justify-center shadow-md border-none cursor-pointer"
     >
       <PlusCircle size={32} color="white" />
     </button>
